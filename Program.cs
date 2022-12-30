c465f132341df56d648f6ada158a22d13f382975
Kindelik <kindelik@yandex.ru>
2022-12-30 14:52:45 +0300

    ReadMe_End

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+System.Console.WriteLine("Введите элементы массива через точку с запятой\n Пример => sdf;12;sinple;gotolag;-2");
+string[] arrayString = Array.ConvertAll<string,string>(Console.ReadLine().Split(';'), elem => { return System.Convert.ToString (elem);});
+System.Console.WriteLine(string.Join(";",arrayString));
